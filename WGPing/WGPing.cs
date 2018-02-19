cc554ba8c5351053358abd2af7f695a051d60f58
Antonio Donato <antonio.donato@cgm.com>
2018-02-19 22:05:13 +0100

    first commit

---
+++ WGPing/WGPing.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.NetworkInformation;
+
+namespace WGPing
+{
+    public class WGPing
+    {
+        public WGPing()
+        {
+        }
+
+        public static bool PingHost(String nameOrAddress)
+        {
+            bool pingable = false;
+            Ping pinger = new Ping();
+            try
+            {
+                PingReply reply = pinger.Send(nameOrAddress);
+                pingable = reply.Status == IPStatus.Success;
+            }
+            catch (PingException)
+            {
+                // Discard PingExceptions and return false;
+            }
+            return pingable;
+        }
+
+
+    }
+}
